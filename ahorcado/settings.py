1ce01605f36ad79aaedc61933945e7e7ae84aad0
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-07-01 14:26:55 -0300

    Add favicon static asset link (#4)

--- ahorcado/settings.py
+++ ahorcado/settings.py
@@ -93,6 +93,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'images']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
